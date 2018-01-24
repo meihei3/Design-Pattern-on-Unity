d166f2f521c2be1b4c5df1da333ca77eaaa8324e
yameholo <ame.aroid.xz5@gmail.com>
2018-01-25 04:53:02 +0900

    add Attack command

---
+++ Assets/Scripts/Commands/KickAttackCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickAttackCommand : Command {
+
+	private GameObject _player;
+	private Animator _animator;
+
+	public KickAttackCommand(GameObject player){
+		_player = player;
+		_animator = _player.GetComponent<Animator> ();
+	}
+
+	public void execute(){
+		_animator.SetBool ("is_attack_2", true);
+	}
+
+}
---
+++ Assets/Scripts/Commands/PunchAttackCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchAttackCommand : Command {
+
+	private GameObject _player;
+	private Animator _animator;
+
+	public PunchAttackCommand(GameObject player){
+		_player = player;
+		_animator = _player.GetComponent<Animator> ();
+	}
+
+	public void execute(){
+		_animator.SetBool ("is_attack_1", true);
+	}
+
+}
